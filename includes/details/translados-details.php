7307ba6fbd81990cf0789b0f0758ec1e43f34c1d
luskha <lucas.limaacesso@gmail.com>
2024-08-08 00:07:30 -0300

    adicionando redirecionamento para translados e passeios

---
+++ includes/details/translados-details.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Translados - Valderir Jeri</title>
+    <link rel="icon" href="/assets/logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/styles.css">
+</head>
+<body>
+    <?php include '../../includes/details/navbar.html'; ?>
+    <?php include '../../includes/details/translados.html'; ?>
+    <?php include '../../includes/details/footer.html'; ?>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="js/carousel.js"></script>
+    <script src="js/reserve.js"></script>
+</body>
+</html>
