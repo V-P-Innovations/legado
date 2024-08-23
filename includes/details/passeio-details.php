25e005c9ea943b2577bc6fbefb03f55b68530c3d
Valney Rodrigues <valneyrodrigues3@gmail.com>
2024-08-23 17:06:11 -0300

    adicionando paginas translados e passeios

--- includes/details/passeio-details.php
+++ includes/details/passeio-details.php
@@ -6,11 +6,11 @@
     <title>Passeios - Valderir Jeri</title>
     <link rel="icon" href="/assets/logo.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../../css/styles.css">
+    <link rel="stylesheet" href="../../css/page.css">
 </head>
 <body>
     <?php include '../../includes/navbar.html'; ?>
-    <?php include '../../includes/passeios.html'; ?>
+    <?php include '../../includes\details\passeios-details.html'; ?>
     <?php include '../../includes/footer.html'; ?>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
