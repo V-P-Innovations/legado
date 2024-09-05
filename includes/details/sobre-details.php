06b61bef49254c04769a4038d7dddfb13ada96eb
Valney Rodrigues <valneyrodrigues3@gmail.com>
2024-09-05 20:36:36 -0300

    estrutando o formato projeto

---
+++ includes/details/sobre-details.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre - Valderir Jeri</title>
+    <link rel="icon" href="/assets/logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/sobre.css">
+</head>
+<body>
+    <?php include '../../includes/navbar.html'; ?>
+    <?php include '../../includes\details\sobre-details.html'; ?>
+    <?php include '../../includes/footer.html'; ?>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -6,23 +6,36 @@
     <title>Valderir Jeri - Transfer e Passeios</title>
     <link rel="icon" href="/assets/logo.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="base.css">
+    <link rel="stylesheet" href="css/home/home.css">
+    <link rel="stylesheet" href="css/home/navbar.css">
+    <link rel="stylesheet" href="css/home/passeios.css">
+    <link rel="stylesheet" href="css/home/translado.css">
+    <link rel="stylesheet" href="css/home/footer.css">
+    <link rel="stylesheet" href="css/home/sobre.css">
 </head>
 <body>
+    <!-- Include PHP files -->
     <?php include 'includes/navbar.html'; ?>
-
     <?php include 'includes/home.html'; ?>
     <?php include 'includes/translados.html'; ?>
     <?php include 'includes/passeios.html'; ?>
     <?php include 'includes/sobre.html'; ?>
-    <?php include 'includes/comentarios.html'; ?>
     <?php include 'includes/footer.html'; ?>
 
+    <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    
+    <!-- Popper.js -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    
+    <!-- Bootstrap JS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="../functions/carousel.js"></script>
-    <script src="../functions/reserve.js"></script>
-    <script src="../functions/fixed-bar.js"></script>
+    
+    <!-- Custom JS -->
+    <script src="functions/carousel.js"></script>
+    <script src="functions/reserve.js"></script>
+    <script src="functions/fixed-bar.js"></script>
 </body>
 </html>
